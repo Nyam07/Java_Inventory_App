298a44a97fc5e290e3f48c6b032fba758b01ad90
Brian Ngatia <bkamunya77@gmail.com>
2023-04-10 14:37:46 +0300

    Moved files to correct package and made changes to ensure project runs successfully

---
+++ src/inventory_app/HashMap.java
@@ -0,0 +1,14 @@
+package inventory_app;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+class HashMap<T0, T1> {
+    
+}
